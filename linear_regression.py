3d80ad0efe60beab975bc514103374c1e02b0223
nadheesh <nadheeshj@gmail.com>
2018-04-23 09:25:56 +0530

    Reformatting and refactoring

--- linear_regression.py
+++ linear_regression.py
@@ -16,19 +16,18 @@ Copyright 2018 Nadheesh Jihan
    Demonstrate the definition of Linear regression model using PyMC3.
 """
 
-import pymc3 as pm
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pymc3 as pm
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from theano import shared
-from sklearn.metrics import mean_squared_error
 
+size = 100  # no of data samples
+true_intercept = 0.5  # intercept of the regression line
+true_slope = 2  # slop (coefficient) of x
 
-size = 100             # no of data samples
-true_intercept = 0.5    # intercept of the regression line
-true_slope = 2          # slop (coefficient) of x
-
-x = np.linspace(0, 1, size) # generate some random values for x in the given range (0,1)
+x = np.linspace(0, 1, size)  # generate some random values for x in the given range (0,1)
 
 # performs simple linear regression
 # y = a + b*x
@@ -39,7 +38,7 @@ true_regression_line = true_intercept + true_slope * x
 y = true_regression_line + np.random.normal(scale=.3, size=size)
 
 # split the data points into train and test split
-x_train, x_test, y_train, y_test, true_y1, true_y2 = train_test_split(x,y,true_regression_line)
+x_train, x_test, y_train, y_test, true_y1, true_y2 = train_test_split(x, y, true_regression_line)
 
 # we use a shared variable from theano to feed the x values into the model
 # this is need for PPC
@@ -49,30 +48,27 @@ shared_x = shared(x_train)
 # training the model
 # model specifications in PyMC3 are wrapped in a with-statement
 with pm.Model() as model:
-
     # Define priors
-    x_coeff = pm.Normal('x', 0, sd=20)                    # prior for coefficient of x
-    intercept = pm.Normal('Intercept', 0, sd=20)            # prior for the intercept
+    x_coeff = pm.Normal('x', 0, sd=20)  # prior for coefficient of x
+    intercept = pm.Normal('Intercept', 0, sd=20)  # prior for the intercept
+    sigma = pm.HalfCauchy('sigma', beta=10)  # prior for the error term of due to the noise
 
-    sigma = pm.HalfCauchy('sigma', beta=10, testval=1.)     # prior for the error term of due to the noise
-
-    mu = intercept + x_coeff * shared_x                      # represent the linear regression relationship
+    mu = intercept + x_coeff * shared_x  # represent the linear regression relationship
 
     # Define likelihood
     likelihood = pm.Normal('y', mu=mu, sd=sigma, observed=y_train)
 
     # Inference!
-    trace = pm.sample(1000, njobs=1)                        # draw 3000 posterior samples using NUTS sampling
-
+    trace = pm.sample(1000, njobs=1)  # draw 3000 posterior samples using NUTS sampling
 
 # predicting the unseen y values
 # uses posterior predictive checks (PPC)
-shared_x.set_value(x_test)                                  # let's set the shared x to the test dataset
-ppc = pm.sample_ppc(trace, model=model, samples=1000)       # performs PPC
-predictions = ppc['y'].mean(axis=0)                         # compute the mean of the samples draws from each new y
+shared_x.set_value(x_test)  # let's set the shared x to the test dataset
+ppc = pm.sample_ppc(trace, model=model, samples=1000)  # performs PPC
+predictions = ppc['y'].mean(axis=0)  # compute the mean of the samples draws from each new y
 
-# now you can check the error
-print("MSE of simple linear regression using bayesian : {0}".format(mean_squared_error(y_test, predictions)))
+# now you can measure the error
+print("\n MSE of simple linear regression using bayesian : {0}\n".format(mean_squared_error(y_test, predictions)))
 
 # plot the traceplot
 pm.traceplot(trace)
@@ -81,10 +77,10 @@ pm.traceplot(trace)
 fig = plt.figure(figsize=(5, 5))
 
 pm.plot_posterior_predictive_glm(trace, samples=100,
-                              label='posterior predictive regression lines')
+                                 label='posterior predictive regression lines')
 plt.plot(x_test, true_y2, label='true regression line', lw=2., c='g')
 
-plt.plot(x_test, y_test, 'x', label='true y', color="blue")
+plt.plot(x_test, true_y2, 'x', label='true y', color="blue")
 plt.plot(x_test, predictions, 'x', label='prediction', color="red")
 plt.title('n = {0}'.format(size))
 plt.legend(loc=0)
@@ -92,5 +88,3 @@ plt.xlabel('x')
 plt.ylabel('y')
 
 plt.show()
-
-
--- logistic_regression.py
+++ logistic_regression.py
@@ -14,12 +14,11 @@ Copyright 2018 Nadheesh Jihan
    limitations under the License.
 """
 
-import pymc3 as pm
 import numpy as np
-import matplotlib.pyplot as plt
+import pymc3 as pm
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from theano import shared, tensor as tt
-from sklearn.metrics import accuracy_score
 
 
 def invlogit(x):
@@ -29,15 +28,15 @@ def invlogit(x):
     :return: sigmoid of x
     """
     if type(x) is np.ndarray:
-        return np.exp(x)/(1+np.exp(x))
+        return np.exp(x) / (1 + np.exp(x))
     return tt.exp(x) / (1 + tt.exp(x))
 
 
-size = 100             # no of data samples
-true_intercept = 0.5    # intercept of the regression line
-true_slope = 2          # slop (coefficient) of x
+size = 100  # no of data samples
+true_intercept = 0.5  # intercept of the regression line
+true_slope = 2  # slop (coefficient) of x
 
-x = np.linspace(0, 1, size) # generate some random values for x in the given range (0,1)
+x = np.linspace(0, 1, size)  # generate some random values for x in the given range (0,1)
 
 # performs simple linear regression
 # y = sigmoid(a + b*x)
@@ -53,7 +52,7 @@ true_regression_line = true_regression_line >= 0.5
 y = y >= 0.5
 
 # split the data points into train and test split
-x_train, x_test, y_train, y_test, true_y1, true_y2 = train_test_split(x,y,true_regression_line)
+x_train, x_test, y_train, y_test, true_y1, true_y2 = train_test_split(x, y, true_regression_line)
 
 # we use a shared variable from theano to feed the x values into the model
 # this is need for PPC
@@ -63,27 +62,25 @@ shared_x = shared(x_train)
 # training the model
 # model specifications in PyMC3 are wrapped in a with-statement
 with pm.Model() as model:
-
     # Define priors
-    x_coeff = pm.Normal('x', 1.5, sd=20)                    # prior for coefficient of x
-    intercept = pm.Normal('Intercept', 0, sd=20)            # prior for the intercept
-
-    sigma = pm.HalfCauchy('sigma', beta=10, testval=1.)     # prior for the error term of due to the noise
+    x_coeff = pm.Normal('x', 0, sd=20)  # prior for coefficient of x
+    intercept = pm.Normal('Intercept', 0, sd=20)  # prior for the intercept
+    sigma = pm.HalfCauchy('sigma', beta=10)  # prior for the error term of due to the noise
 
     reg = intercept + tt.dot(shared_x, x_coeff)
-    p = pm.Deterministic("p", invlogit(reg))                # represent the logistic regression relationship
+    p = pm.Deterministic("p", invlogit(reg))  # represent the logistic regression relationship
 
     # Define likelihood
     likelihood = pm.Bernoulli('y', p=p, observed=y_train)
 
     # Inference!
-    trace = pm.sample(1000)                        # draw 3000 posterior samples using NUTS sampling
+    trace = pm.sample(1000)  # draw 3000 posterior samples using NUTS sampling
 
 # predicting the unseen y values
 # uses posterior predictive checks (PPC)
-shared_x.set_value(x_test)                                  # let's set the shared x to the test dataset
-ppc = pm.sample_ppc(trace, model=model, samples=1000)       # performs PPC
-predictions = ppc['y'].mean(axis=0)                         # compute the mean of the samples draws from each new y
+shared_x.set_value(x_test)  # let's set the shared x to the test dataset
+ppc = pm.sample_ppc(trace, model=model, samples=1000)  # performs PPC
+predictions = ppc['y'].mean(axis=0)  # compute the mean of the samples draws from each new y
 
 predictions = predictions >= 0.5
 # now you can check the error
